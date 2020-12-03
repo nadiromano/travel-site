4b780839ce9d29b55fa6b3a6d4a70bbbb658dacd
Giuseppe <giuseppenadir.romano@gmail.com>
2020-12-03 12:18:07 +0100

    modal completato

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,8 @@
 import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
-import StickyHeader from './modules/StickyHeader'
+import StickyHeader from './modules/StickyHeader';
+
 
 
 let stickyHeader = new StickyHeader()
@@ -9,6 +10,21 @@ new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
 
 let mobileMenu = new MobileMenu();
+let modal
+
+document.querySelectorAll(".open-modal").forEach(el => {
+    el.addEventListener("click", e => {
+        e.preventDefault()
+        if(typeof modal == "undefined") {
+            import(/* webpackChunkName: "modal" */'./modules/Modal').then(x => {
+                modal = new x.default()
+                setTimeout(() => modal.openTheModal(), 20)
+            }).catch(() => console.log("qualcosa non va"))
+        } else {
+            modal.openTheModal()
+        }
+    })
+})
 
 
 if (module.hot) {
